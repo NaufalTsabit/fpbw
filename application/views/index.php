70a941aabed280f974af5baaaff601078dac8e67
NaufalTsabit <naufaltsabit10@gmail.com>
2018-05-12 22:59:01 +0700

    Bootstrap

--- application/views/index.php
+++ application/views/index.php
@@ -4,7 +4,7 @@
 		<!-- Mobile Specific Meta -->
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<!-- Favicon-->
-		<link rel="shortcut icon" href="img/fav.png">
+		<link rel="shortcut icon" href="assets/img/fav.png">
 		<!-- Author Meta -->
 		<meta name="author" content="codepixer">
 		<!-- Meta Description -->
@@ -20,15 +20,15 @@
 			<!--
 			CSS
 			============================================= -->
-			<link rel="stylesheet" href="css/linearicons.css">
-			<link rel="stylesheet" href="css/font-awesome.min.css">
-			<link rel="stylesheet" href="css/bootstrap.css">
-			<link rel="stylesheet" href="css/magnific-popup.css">
-			<link rel="stylesheet" href="css/nice-select.css">					
-			<link rel="stylesheet" href="css/animate.min.css">
+			<link rel="stylesheet" href="assets/css/linearicons.css">
+			<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+			<link rel="stylesheet" href="assets/css/bootstrap.css">
+			<link rel="stylesheet" href="assets/css/magnific-popup.css">
+			<link rel="stylesheet" href="assets/css/nice-select.css">					
+			<link rel="stylesheet" href="assets/css/animate.min.css">
 			<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">			
-			<link rel="stylesheet" href="css/owl.carousel.css">
-			<link rel="stylesheet" href="css/main.css">
+			<link rel="stylesheet" href="assets/css/owl.carousel.css">
+			<link rel="stylesheet" href="assets/css/main.css">
 		</head>
 		<body>
 
@@ -36,11 +36,11 @@
 			    <div class="container">
 			    	<div class="row align-items-center justify-content-between d-flex">
 				      <div id="logo">
-				        <a href="index.html"><img src="img/logo.png" alt="" title="" /></a>
+				        <a href="index.html"><img src="assets/img/logo.png" alt="" title="" /></a>
 				      </div>
 				      <nav id="nav-menu-container">
 				        <ul class="nav-menu">
-				          <li class="menu-active"><a href="index.html">Home</a></li>
+				          <li class="menu-active"><a href="#">Home</a></li>
 				          <li><a href="about.html">About</a></li>
 				          <li><a href="cars.html">Cars</a></li>
 				          <li><a href="service.html">Service</a></li>
@@ -217,7 +217,7 @@
 				<div class="container-fluid">				
 					<div class="row justify-content-center align-items-center">
 						<div class="col-lg-6 no-padding home-about-left">
-							<img class="img-fluid" src="img/about-img.jpg" alt="">
+							<img class="img-fluid" src="assets/img/about-img.jpg" alt="">
 						</div>
 						<div class="col-lg-6 no-padding home-about-right">
 							<h1>Globally Connected <br>
@@ -266,7 +266,7 @@
 								<a class="text-uppercase primary-btn" href="#">Book This Car Now</a>
 							</div>
 							<div class="col-lg-6 model-right">
-								<img class="img-fluid" src="img/car.jpg" alt="">
+								<img class="img-fluid" src="assets/img/car.jpg" alt="">
 							</div>
 						</div>
 						<div class="row align-items-center single-model item">
@@ -287,7 +287,7 @@
 								<a class="text-uppercase primary-btn" href="#">Book This Car Now</a>
 							</div>
 							<div class="col-lg-6 model-right">
-								<img class="img-fluid" src="img/car.jpg" alt="">
+								<img class="img-fluid" src="assets/img/car.jpg" alt="">
 							</div>
 						</div>
 						<div class="row align-items-center single-model item">
@@ -308,7 +308,7 @@
 								<a class="text-uppercase primary-btn" href="#">Book This Car Now</a>
 							</div>
 							<div class="col-lg-6 model-right">
-								<img class="img-fluid" src="img/car.jpg" alt="">
+								<img class="img-fluid" src="assets/img/car.jpg" alt="">
 							</div>
 						</div>												
 					</div>
@@ -485,7 +485,7 @@
 					<div class="row">
 						<div class="col-lg-3 col-md-6 single-blog">
 							<div class="thumb">
-								<img class="img-fluid" src="img/b1.jpg" alt="">								
+								<img class="img-fluid" src="assets/img/b1.jpg" alt="">								
 							</div>
 							<p class="date">10 Jan 2018</p>
 							<a href="blog-single.html"><h4>Addiction When Gambling
@@ -500,7 +500,7 @@
 						</div>
 						<div class="col-lg-3 col-md-6 single-blog">
 							<div class="thumb">
-								<img class="img-fluid" src="img/b2.jpg" alt="">								
+								<img class="img-fluid" src="assets/img/b2.jpg" alt="">								
 							</div>
 							<p class="date">10 Jan 2018</p>
 							<a href="blog-single.html"><h4>Addiction When Gambling
@@ -515,7 +515,7 @@
 						</div>
 						<div class="col-lg-3 col-md-6 single-blog">
 							<div class="thumb">
-								<img class="img-fluid" src="img/b3.jpg" alt="">								
+								<img class="img-fluid" src="assets/img/b3.jpg" alt="">								
 							</div>
 							<p class="date">10 Jan 2018</p>
 							<a href="blog-single.html"><h4>Addiction When Gambling
@@ -530,7 +530,7 @@
 						</div>
 						<div class="col-lg-3 col-md-6 single-blog">
 							<div class="thumb">
-								<img class="img-fluid" src="img/b4.jpg" alt="">								
+								<img class="img-fluid" src="assets/img/b4.jpg" alt="">								
 							</div>
 							<p class="date">10 Jan 2018</p>
 							<a href="blog-single.html"><h4>Addiction When Gambling
@@ -616,33 +616,30 @@
 							</div>
 						</div>	
 						<p class="mt-50 mx-auto footer-text col-lg-12">
-							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 						</p>											
 					</div>
 				</div>
 			</footer>	
 			<!-- End footer Area -->		
 
-			<script src="js/vendor/jquery-2.2.4.min.js"></script>
+			<script src="assets/js/vendor/jquery-2.2.4.min.js"></script>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-			<script src="js/vendor/bootstrap.min.js"></script>			
+			<script src="assets/js/vendor/bootstrap.min.js"></script>			
 			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
 			<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>			
-  			<script src="js/easing.min.js"></script>			
-			<script src="js/hoverIntent.js"></script>
-			<script src="js/superfish.min.js"></script>	
-			<script src="js/jquery.ajaxchimp.min.js"></script>
-			<script src="js/jquery.magnific-popup.min.js"></script>	
-			<script src="js/owl.carousel.min.js"></script>			
-			<script src="js/jquery.sticky.js"></script>
-			<script src="js/jquery.nice-select.min.js"></script>	
-			<script src="js/waypoints.min.js"></script>
-			<script src="js/jquery.counterup.min.js"></script>					
-			<script src="js/parallax.min.js"></script>		
-			<script src="js/mail-script.js"></script>	
-			<script src="js/main.js"></script>	
+  			<script src="assets/js/easing.min.js"></script>			
+			<script src="assets/js/hoverIntent.js"></script>
+			<script src="assets/js/superfish.min.js"></script>	
+			<script src="assets/js/jquery.ajaxchimp.min.js"></script>
+			<script src="assets/js/jquery.magnific-popup.min.js"></script>	
+			<script src="assets/js/owl.carousel.min.js"></script>			
+			<script src="assets/js/jquery.sticky.js"></script>
+			<script src="assets/js/jquery.nice-select.min.js"></script>	
+			<script src="assets/js/waypoints.min.js"></script>
+			<script src="assets/js/jquery.counterup.min.js"></script>					
+			<script src="assets/js/parallax.min.js"></script>		
+			<script src="assets/js/mail-script.js"></script>	
+			<script src="assets/js/main.js"></script>	
 		</body>
 	</html>
 
